164ad253c60d62bd9f42e6d682309970b4196b12
David Hérault <dherault@gmail.com>
2017-01-04 13:30:00 +0100

    fix airbnb-base module name

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## 0.1.1
+## 0.1.1 and 0.1.2
 
 **Bug fixes:**
 - Use `require.resolve` to resolve airbnb-base dependency.
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: require.resolve('airbnb-base'),
+  extends: require.resolve('eslint-config-airbnb-base'),
   rules: {
     'arrow-parens': [2, 'as-needed'],
     'brace-style': [2, 'stroustrup', { allowSingleLine: true }],
